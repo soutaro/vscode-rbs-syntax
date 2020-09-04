bae7c751fffdcdd9c56415926c4686ee84cbc827
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-09-04 16:36:38 +0900

    Improve formatting of | after def and new line after |

--- src/extension.ts
+++ src/extension.ts
@@ -9,29 +9,51 @@ import {
 	Range,
 	Position,
 	ProviderResult,
+	TextLine,
 } from 'vscode';
 
 class InsertOrDeleteBarProvider implements OnTypeFormattingEditProvider {
+  matchDef(line: TextLine): RegExpMatchArray | null {
+		return line.text.match(/^(\s*)(def [^:]+):/)
+	}
+
+	matchBar(line: TextLine): RegExpMatchArray | null {
+		return line.text.match(/^(\s*\|\s*)$/)
+	}
+
+	formatBarAfterDef(document: TextDocument, position: Position): ProviderResult<TextEdit[]> | null {
+		const prevLine = document.lineAt(position.line - 1)
+		const def = this.matchDef(document.lineAt(position.line - 1))
+		if (!def) return
+
+		const indentSize = def[2].length;
+		const insertBar = TextEdit.insert(position, ' '.repeat(indentSize) + '| ')
+		return Promise.resolve([insertBar])
+	}
+
+	formatLineAfterBar(document: TextDocument, position: Position): ProviderResult<TextEdit[]> | null {
+		const prevLine = document.lineAt(position.line - 1)
+		const bar = this.matchBar(prevLine)
+		if (!bar) return
+
+		const prevPrevLine = document.lineAt(position.line - 2)
+		const def = this.matchDef(prevPrevLine)
+		if (!def) return
+
+		const deleteEmptyType = TextEdit.delete(prevLine.range)
+		const indentRange = document.lineAt(position.line).range
+		const deleteIndent = TextEdit.delete(indentRange)
+		const insertIndent = TextEdit.insert(new Position(position.line, 0), ' '.repeat(def[1].length))
+
+		return Promise.resolve([deleteEmptyType, deleteIndent, insertIndent])
+	}
+
 	provideOnTypeFormattingEdits(document: TextDocument, position: Position, ch: string, options: FormattingOptions, token: CancellationToken): ProviderResult<TextEdit[]> {
 		if (document.lineAt(position.line).text.substring(position.character).length) {
 			return
 		}
 
-		const prevLine = document.lineAt(position.line - 1)
-		const def = prevLine.text.match(/^(\s*def [^:]+):/)
-		if (def) {
-			const indentSize = def[1].length - options.tabSize;
-			const insertBar = TextEdit.insert(position, ' '.repeat(indentSize) + '| ')
-			return Promise.resolve([insertBar])
-		}
-
-		if (/^(\s*\|\s*)$/.test(prevLine.text)) {
-			const deleteEmptyType = TextEdit.delete(prevLine.range)
-			const indentRange = document.lineAt(position.line).range
-			const deleteIndent = TextEdit.delete(indentRange)
-			const insertIndent = TextEdit.insert(new Position(position.line, 0), ' '.repeat(options.tabSize))
-			return Promise.resolve([deleteEmptyType, deleteIndent, insertIndent])
-		}
+		return this.formatBarAfterDef(document, position) || this.formatLineAfterBar(document, position)
 	}
 }
 
